d34d8451b354c8612d4b8592f306f521007ecd4c
fsyk328 <fishskin328@gmail.com>
2017-03-27 18:19:36 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/webapp/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.mycompany.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class IndexController {
+
+   @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    
+    @RequestMapping("login")
+    public String login() {
+        return "login";
+    }
+    
+    @RequestMapping("register")
+    public String register() {
+        return "register";
+    }
+}
