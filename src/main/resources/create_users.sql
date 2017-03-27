c9ce80d1f976c2b34b840fd31437bfc5294dc704
s1158601 <s1158601@540EW64A.studentdmn.ouhk.edu.hk>
2017-03-27 17:17:04 +0800

    new commit

--- src/main/resources/create_users.sql
+++ src/main/resources/create_users.sql
@@ -1,29 +1,71 @@
+--DROP TABLE users,user_roles,topic,reply,attachment,poll,poll_ans,vote;
+
 CREATE TABLE users (
+    id INTEGER NOT NULL,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    PRIMARY KEY (username)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE user_roles (
-    user_role_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    username VARCHAR(50) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    PRIMARY KEY (user_role_id),
-    FOREIGN KEY (username) REFERENCES users(username)
+    id INTEGER  PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
 
-INSERT INTO users VALUES ('demo', 'demo');
-INSERT INTO user_roles(username, role) VALUES ('demo', 'ROLE_ADMIN');
+CREATE TABLE topic (
+    id INTEGER  PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    msg VARCHAR(200),
+    category VARCHAR(20) NOT NULL
+);
 
-INSERT INTO users VALUES ('keith', 'keithpw');
-INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_USER');
-INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_ADMIN');
+CREATE TABLE reply (
+    id INTEGER  PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    msg VARCHAR(200),
+    topic_id INTEGER NOT NULL
+);
 
-INSERT INTO users VALUES ('andrew', 'andrewpw');
-INSERT INTO user_roles(username, role) VALUES ('andrew', 'ROLE_ADMIN');
+CREATE TABLE attachment (
+    id INTEGER PRIMARY KEY,
+    topic_id INTEGER,
+    reply_id INTEGER,
+    user_id INTEGER,
+    attachment varchar(200)
+);
 
-INSERT INTO users VALUES ('maria', 'mariapw');
-INSERT INTO user_roles(username, role) VALUES ('maria', 'ROLE_USER');
+CREATE TABLE poll (
+    id INTEGER  PRIMARY KEY,
+    question VARCHAR(100)
+);
 
-INSERT INTO users VALUES ('oliver', 'oliverpw');
-INSERT INTO user_roles(username, role) VALUES ('oliver', 'ROLE_USER');
+CREATE TABLE poll_ans (
+    id INTEGER  PRIMARY KEY,
+    poll_id INTEGER NOT NULL,
+    ans VARCHAR(100)
+);
+
+CREATE TABLE vote (
+    id INTEGER  PRIMARY KEY,
+    poll_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    ans_id INTEGER NOT NULL
+);
+
+INSERT INTO users VALUES (1,'demo', 'demo');
+INSERT INTO user_roles(id, user_id, role) VALUES ((SELECT MAX(ID) FROM users),1, 'ROLE_ADMIN');
+
+
+/*INSERT INTO users VALUES (2,'keith', 'keithpw');
+INSERT INTO user_roles(user_id, role) VALUES (2, 'ROLE_USER');
+INSERT INTO user_roles(user_id, role) VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO users VALUES (3,'andrew', 'andrewpw');
+INSERT INTO user_roles(user_id, role) VALUES (3, 'ROLE_ADMIN');
+
+INSERT INTO users VALUES (4,'maria', 'mariapw');
+INSERT INTO user_roles(user_id, role) VALUES (4, 'ROLE_USER');
+
+INSERT INTO users VALUES (5,'oliver', 'oliverpw');
+INSERT INTO user_roles(user_id, role) VALUES (5, 'ROLE_USER');*/
