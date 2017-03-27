f162b1af351e6281171353c7f20ec0c46c86c334
s1158601 <s1158601@540EW64A.studentdmn.ouhk.edu.hk>
2017-03-27 15:00:58 +0800

    new commit

---
+++ src/main/java/com/mycompany/webapp/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.mycompany.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public View index() {
+        return new RedirectView("/ticket/list", true);
+    }
+    
+    @RequestMapping("login")
+    public String login() {
+        return "login";
+    }
+    
+}
