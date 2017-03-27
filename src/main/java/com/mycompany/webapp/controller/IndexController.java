eb458653e00cc4ca045d791a18cea8636f7011fc
fsyk328 <fishskin328@gmail.com>
2017-03-27 17:52:52 +0800

    Update IndexController.java

--- src/main/java/com/mycompany/webapp/controller/IndexController.java
+++ src/main/java/com/mycompany/webapp/controller/IndexController.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public View index() {
-        return new RedirectView("/ticket/list", true);
+   @RequestMapping("/")
+    public String index() {
+        return "index";
     }
     
     @RequestMapping("login")
